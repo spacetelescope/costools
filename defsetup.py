5cfc0909cc85deeb29dd0e1fa5a7cf346f0a6384
hodge <hodge@stsci.edu>
2011-04-15 17:55:50 +0000

    Changed 'reftools' to 'costools'.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/costools@12513 fe389314-cf27-0410-b35b-8c050e845b92

--- defsetup.py
+++ defsetup.py
@@ -1,12 +1,12 @@
 from __future__ import division # confidence high
 
-pkg = "reftools"
+pkg = "costools"
 
 setupargs = {
 
     'version' :         '1.0',
-    'description' :     "Reference File Python Tools",
-    'author' :          "Warren Hack, Nadezhda Dencheva",
+    'description' :     "Python Modules for COS",
+    'author' :          "Warren Hack, Nadezhda Dencheva, Phil Hodge",
     'author_email' :    "help@stsci.edu",
     'license' :         "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
     'data_files' :      [( pkg+"/pars", ['lib/pars/*']), (pkg, ['lib/*.help']), (pkg, ['lib/LICENSE.txt'])],
