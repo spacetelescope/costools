b34118b89f88980237c8251dc5243d8d875411e5
hodge <hodge@stsci.edu>
2011-07-06 17:47:15 +0000

    Import parseinput and teal from stsci.pytools, not pytools.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/costools@13320 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/costools/timefilter.py
+++ lib/costools/timefilter.py
@@ -41,7 +41,7 @@ import os
 import sys
 import numpy as np
 import pyfits
-from pytools import parseinput, teal
+from stsci.pytools import parseinput, teal
 from calcos import ccos
 from calcos import calcosparam, cosutil
 import saamodel
