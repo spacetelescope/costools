55537a6e068072f862377536579ff8799d91ede0
hodge <hodge@stsci.edu>
2013-01-23 21:48:31 +0000

    The timefilter module is imported, and the module names (just timefilter for now) should be printed when costools is imported. See Trac #975.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/costools@21716 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/costools/__init__.py
+++ lib/costools/__init__.py
@@ -16,7 +16,15 @@ else :
     __version__ = '1.1'
 
 try:
-    from costools.svninfo import (__svn_version__, __full_svn_info__,
-                                  __setup_datetime__)
+    from costools.svninfo import(__svn_version__, __full_svn_info__,
+                                 __setup_datetime__)
 except ImportError:
     pass
+
+import timefilter
+
+# These lines allow TEAL to print out the names of TEAL-enabled tasks
+# upon importing this package.
+import os
+from stsci.tools import teal
+teal.print_tasknames(__name__, os.path.dirname(__file__))
