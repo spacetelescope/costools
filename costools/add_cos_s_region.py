14a96ee0ef2929eb412185e3e95c76e85d5ba8f6
Robert Jedrzejewski <rij@stsci.edu>
2022-11-30 09:51:03 -0500

    Add argparse processing of rootnames, some cleanup of code

--- costools/add_cos_s_region.py
+++ costools/add_cos_s_region.py
@@ -8,17 +8,17 @@ import argparse
 from calcos import cosutil
 from astropy.io import fits
 
-def main(dry_run=False):
-    """Add the S-region keyword to all COS FITS files in the current
+def main(rootnames, dry_run=False):
+    """Add the S-region keyword to COS FITS files in the current
     directory that meet the following criteria:
 
-    Filename ends with one of ['rawtag.fits',
-                               'rawtag_a.fits',
-                               'rawtag_b.fits',
-                               'rawaccum.fits',
-                               'rawaccum_a.fits',
-                               'rawaccum_b.fits',
-                               'rawacq.fits']
+    ['rootname_rawtag.fits',
+     'rootname_rawtag_a.fits',
+     'rootname_rawtag_b.fits',
+     'rootname_rawaccum.fits',
+     'rootname_rawaccum_a.fits',
+     'rootname_rawaccum_b.fits',
+     'rootname_rawacq.fits']
 
     The S_REGION keyword will have the value
     'CIRCLE ICRS ra_aper dec_aper diameter'
@@ -30,6 +30,20 @@ def main(dry_run=False):
     header
 
     """
+    if rootnames is None:
+        cosutil.printError("No rootnames specified")
+        return
+
+    files_to_process = get_files_to_process(rootnames)
+    for input_file in files_to_process:
+        add_s_region(input_file, dry_run)
+    return
+
+def get_files_to_process(rootnames):
+    """Create a list of files to process from the list of rootnames
+
+    """
+
     endings = ['rawtag.fits',
                'rawtag_a.fits',
                'rawtag_b.fits',
@@ -37,15 +51,18 @@ def main(dry_run=False):
                'rawaccum_a.fits',
                'rawaccum_b.fits',
                'rawacq.fits']
-
-    fitsfiles = glob.glob('*.fits')
-    for input_file in fitsfiles:
-        for ending in endings:
-            if input_file.endswith(ending):
-                add_s_region(input_file, dry_run)
-    return
+    file_list = []
+    for rootname in rootnames:
+        fitslist = glob.glob(rootname.lower() + '*.fits')
+        for input_file in fitslist:
+            for ending in endings:
+                if input_file.endswith(ending):
+                    file_list.append(input_file)
+    return file_list
 
 def add_s_region(input_file, dry_run):
+    """Add the computed S_REGION to the keyword
+    """
 
     open_mode = 'readonly'
     if not dry_run: open_mode = 'update'
@@ -58,14 +75,16 @@ def add_s_region(input_file, dry_run):
         success = False
         for n_ext, extension in enumerate(f1[1:]):
             exthdr = extension.header
-            if exthdr['extname'] in ['SCI', 'EVENTS', 'ACQ']:
+            extname = exthdr['EXTNAME']
+            extver = exthdr['EXTVER']
+            if extname in ['SCI', 'EVENTS', 'ACQ']:
                 try:
                     ra_aper = exthdr['RA_APER']
                     dec_aper = exthdr['DEC_APER']
                     pa_aper = exthdr['PA_APER']
                 except KeyError:
                     cosutil.printWarning('One or more of RA_APER, DEC_APER, PA_APER is not present')
-                    cosutil.printContinuation('in extension {} header of file {}'.format(n_ext, input_file))
+                    cosutil.printContinuation('in extension {} header of file {}'.format(n_ext+1, input_file))
                     continue
                 diameter = 2.5 / 3600.0
                 s_region = 'CIRCLE ICRS {0:.8f} {1:.7f} {2:.8f}'.format(ra_aper, dec_aper, diameter)
@@ -73,13 +92,13 @@ def add_s_region(input_file, dry_run):
                     try:
                         existing_s_region = exthdr['S_REGION']
                         exthdr['S_REGION'] = s_region
-                        cosutil.printMsg("Using existing S_REGION keyword in file {}".format(input_file))
+                        cosutil.printMsg("Using existing S_REGION keyword in {}[{}, {}]".format(input_file, extname, extver))
                     except KeyError:
-                        ext1hdr.insert('PA_APER', ('S_REGION', s_region, 'Spatial extent of the observation'), after=True)
-                        cosutil.printMsg('Added S_REGION keyword to {}'.format(input_file))
+                        exthdr.insert('PA_APER', ('S_REGION', s_region, 'Spatial extent of the observation'), after=True)
+                        cosutil.printMsg('Added S_REGION keyword to {}[{}, {}]'.format(input_file, extname, extver))
                     cosutil.printMsg("Setting value to '{}'".format(s_region))
                 else:
-                    cosutil.printMsg("S_REGION for file {} = '{}'".format(input_file, s_region))
+                    cosutil.printMsg("S_REGION for {}[{}, {}] = '{}'".format(input_file, extname, extver, s_region))
                     cosutil.printMsg("Dry-run - no changes made to science header")
                 success = True
         if not success:
@@ -91,10 +110,12 @@ if __name__ == '__main__':
     """Add S_REGION value to raw data headers"""
     )
 
+    parser.add_argument('rootnames', nargs='+',
+        help='Rootnames to be processed')
+
     parser.add_argument(
         '--dry_run', action='store_true',
         help="Calculate S_REGION value, but don't write to data header[s]")
 
     args = parser.parse_args()
-
-    main(dry_run=args.dry_run)
+    main(args.rootnames, dry_run=args.dry_run)
